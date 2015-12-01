8b90a5a5939f7d262a41ba4f217f2402c03e3f09
Sérgio Ramos <mail@sergioramos.me>
2015-12-01 00:39:01 +0000

    fix infinite loop

--- src/worker-stream.js
+++ src/worker-stream.js
@@ -1,5 +1,9 @@
 var Duplex = require('readable-stream').Duplex
+var isPlainObject = require('lodash.isplainobject');
+var isFunction = require('lodash.isfunction');
 var inherits = require('inherits');
+var bind = require('lodash.bind');
+
 
 var WorkerStream = module.exports = function(worker) {
   if (!(this instanceof WorkerStream)) {
@@ -10,10 +14,10 @@ var WorkerStream = module.exports = function(worker) {
     objectMode: true
   });
 
-  this.worker = worker || self;
+  this._worker = worker || self;
   this._holding = [];
 
-  this.worker.onmessage = this._write;
+  this._worker.onmessage = bind(this._write, this);
 };
 
 inherits(WorkerStream, Duplex);
@@ -36,7 +40,9 @@ WorkerStream.prototype._read = function(size, tryit) {
 };
 
 WorkerStream.prototype._write = function(chunk, enc, fn) {
-  this.worker.postMessage(chunk);
+  if (isPlainObject(chunk)) {
+    this._worker.postMessage(chunk);
+  }
 
   if (this._readableState.pipesCount) {
     this._holding.push(chunk);
@@ -44,5 +50,7 @@ WorkerStream.prototype._write = function(chunk, enc, fn) {
 
   this._read(this._readableState.highWaterMark, true);
 
-  fn();
+  if (isFunction(fn)) {
+    fn();
+  }
 };
